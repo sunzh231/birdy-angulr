951d3dffa4d0d9212042cf263f04b0f6151c4c97
孙宗衡 <sunzh@huntor.cn>
2017-04-20 20:43:17 +0800

    src backup

---
+++ src_backup/l10n/zn_CH.js
@@ -0,0 +1,76 @@
+{
+  "header" : {
+    "navbar" : {
+      "UPLOAD" : "上传",
+      "new" : {
+        "NEW" : "消息",
+        "PROJECT" : "Projects",
+        "TASK" : "任务",
+        "USER" : "用户",
+        "EMAIL" : "电子邮件"
+      },
+      "NOTIFICATIONS" : "通知"
+    }
+  },
+  "aside" : {
+    "nav" : {
+      "HEADER" : "通知",
+      "DASHBOARD" : "主页",
+      "CALENDAR" : "日历",
+      "EMAIL" : "电子邮件",
+      "WIDGETS" : "部件",
+      "core": {
+        "BASE_FEATURE": "基础功能",
+        "channel": {
+          "CHANNEL": "渠道管理"
+        }
+      },
+      "components" : {
+        "COMPONENTS" : "组件",
+        "ui_kits" : {
+          "UI_KITS" : "UI Kits",
+          "BUTTONS" : "Buttons",
+          "ICONS" : "Icons",
+          "GRID" : "Grid",
+          "BOOTSTRAP" : "Bootstrap",
+          "SORTABLE" : "Sortable",
+          "PORTLET" : "Portlet",
+          "TIMELINE" : "Timeline",
+          "VECTOR_MAP" : "Vector Map"
+        },
+        "table" : {
+          "TABLE" : "Table",
+          "TABLE_STATIC" : "Table static",
+          "DATATABLE" : "Datatable",
+          "FOOTABLE" : "Footable"
+        },
+        "form" : {
+          "FORM" : "Form",
+          "FORM_ELEMENTS" : "Form elements",
+          "FORM_VALIDATION" : "Form validation",
+          "FORM_WIZARD" : "Form wizard"
+        },
+        "CHART" : "Chart",
+        "pages" : {
+          "PAGES" : "Pages",
+          "PROFILE" : "Profile",
+          "POST" : "Post",
+          "SEARCH" : "Search",
+          "INVOICE" : "Invoice",
+          "LOCK_SCREEN" : "Lock screen",
+          "SIGNIN" : "Signin",
+          "SIGNUP" : "Signup",
+          "FORGOT_PASSWORD" : "Forgot password",
+          "404" : "404"
+        }
+      },
+      "your_stuff" : {
+        "YOUR_STUFF": "个人中心",
+        "PROFILE" : "我的信息",
+        "DOCUMENTS" : "帮助文档"
+      }
+    },
+    "MILESTONE" : "Milestone",
+    "RELEASE" : "Release"
+  }
+}
